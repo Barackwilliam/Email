ef680807e0195c7e300e7416c447931ee513e7c6
barackwilliam <barackwilliam12@gmail.com>
2025-04-13 09:22:59 +0300

    allow empty email and phone field

--- Email_app/models.py
+++ Email_app/models.py
@@ -6,8 +6,8 @@ class EmailEntry(models.Model):
     region = models.CharField(max_length=100)
     district = models.CharField(max_length=100)
     school_name = models.CharField(max_length=200)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(blank=True,null=True)
+    phone_number = models.CharField(max_length=20,blank=True,null=True)
 
     def __str__(self):
         return self.school_name
