06d4a2cc482adca1e8e66160181ff8e5b70d3329
barackwilliam <barackwilliam12@gmail.com>
2025-04-13 09:37:06 +0300

    allow empty email and phone field

--- Email_app/forms.py
+++ Email_app/forms.py
@@ -1,3 +1,11 @@
+
+from django import forms
+from django.contrib.auth.models import User
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+
 from django import forms
 from .models import EmailEntry
 
@@ -8,17 +16,13 @@ class EmailEntryForm(forms.ModelForm):
         
     def __init__(self, *args, **kwargs):
         super(EmailEntryForm, self).__init__(*args, **kwargs)
-        # Adding custom CSS classes and other attributes
+        # Adding custom CSS classes and placeholders
         self.fields['region'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter region'})
         self.fields['district'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter district'})
         self.fields['school_name'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter school name'})
         self.fields['email'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter email address'})
         self.fields['phone_number'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter phone number'})
-
-
-from django import forms
-from django.contrib.auth.models import User
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+        
+        # Make email and phone_number optional in the form
+        self.fields['email'].required = False
+        self.fields['phone_number'].required = False
